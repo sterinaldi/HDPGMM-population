130c83943ae72901a8c6c23a41f1734c06bd12f9
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-07-28 09:15:14 +0200

    update

--- parest_test.py
+++ parest_test.py
@@ -23,8 +23,11 @@ def gauss(x, x0, s):
 def bimodal(x,x0,s0,x1,s1):
     return (np.exp(-((x-x0)**2/(2*s0**2)))/(np.sqrt(2*np.pi)*s0) + np.exp(-((x-x1)**2/(2*s1**2)))/(np.sqrt(2*np.pi)*s1))/2.
 
+def four_g(x,x0,x1,x2,x3,s0,s1,s2,s3,b0,b1,b2,b3):
+    return (b0*np.exp(-((x-x0)**2/(2*s0**2)))/(np.sqrt(2*np.pi)*s0) + b1*np.exp(-((x-x1)**2/(2*s1**2)))/(np.sqrt(2*np.pi)*s1)+ b2*np.exp(-((x-x2)**2/(2*s2**2)))/(np.sqrt(2*np.pi)*s2)+ b3*np.exp(-((x-x3)**2/(2*s3**2)))/(np.sqrt(2*np.pi)*s3))/(4*(b1+b2+b3+b0))
+
 def logPrior(*args):
-    return 0
+    return 0#-args[1]-args[0]
  
 #
 alpha = 1.1
@@ -32,18 +35,31 @@ mmax = 75
 mmin = 15
 lmax = 5
 lmin = 5
-mu = 30
-sigma = 6
+mu = 50
+sigma = 4
 a = 20
+g = 20
 #true_vals = [alpha, mmax, mmin, lmax, lmin, a, 1]
-true_vals = [mu, sigma, a, 1]
+#true_vals = [mu, sigma, a, 1]
 #true_vals = [50, 2, 100, 2.5, a, 1]
+#true_vals = [25,4,55,5, a, g, 1]
+#true_vals = [30,3, 60, 5, a, g,1]
+true_vals  = [50,4,a,g,1]
+#true_vals = [38, 54, 45,60,6,4,5,7,0.4,0.1,0.2,0.3]
+#true_vals = [60,54,45,38,7,4,5,6,0.3,0.1,0.2,0.4, a,1]
 
-samp_file = '/Users/stefanorinaldi/Documents/mass_inference/DP/reconstructed_events/posteriors/posterior_functions_event_1.pkl'
+#samp_file = '/Users/stefanorinaldi/Documents/mass_inference/bimodal_good/mass_function/all_samples.pkl'
+#samp_file = '/Users/stefanorinaldi/Documents/mass_inference/selfunc_gaussian/mass_function/astro_posteriors.pkl'
+samp_file = '/Users/stefanorinaldi/Documents/mass_inference/DPGMM/reconstructed_events/posteriors/posterior_functions_gaussian.pkl'
 openfile  = open(samp_file, 'rb')
 samples   = pickle.load(openfile)
 openfile.close()
 
+
+#rec_file = '/Users/stefanorinaldi/Documents/mass_inference/bimodal_good/mass_function/log_joint_obs_prob_mf.txt'
+rec_file = '/Users/stefanorinaldi/Documents/mass_inference/DPGMM/reconstructed_events/rec_prob/log_rec_prob_gaussian.txt'
+#rec_file = '/Users/stefanorinaldi/Documents/mass_inference/selfunc_gaussian/mass_function/log_rec_prob_mf.txt'
+rec = np.genfromtxt(rec_file, names = True)
 
 x = np.linspace(20, 40,400)
 dx = x[1]-x[0]
@@ -97,36 +113,48 @@ out_folder  = '/Users/stefanorinaldi/Documents/parametric/DP'
 #names = ['mu_1', 'sigma_1', 'mu_2', 'sigma_2']
 #bounds = [[40,60], [1,4], [90, 110], [1,4]]
 #labels = ['\mu_1', '\sigma_1', '\mu_2', '\sigma_2']
-names = ['mu', 'sigma']
-bounds = [[10,40], [3,7]]
-labels = ['\mu', '\sigma']
+names = ['mu1', 'sigma1']#, 'mu2','sigma2']
+bounds = [[40,60], [2,10]]#,[40,70],[2,7]]
+labels = ['\mu_1', '\sigma_1']#,'\mu_2','\sigma_2']
+selected_model = gauss
+
+#names  = ['mu1','mu2','mu3', 'mu4', 's1', 's2', 's3', 's4', 'b1', 'b2','b3','b4']
+#bounds = [[58,70], [50,58], [40,50],[30,40], [1,8], [1,8], [1,8],[1,8], [0,1],[0,1],[0,1],[0,1]]
+#labels = ['\mu_1','\mu_2','\mu_3', '\mu_4', '\sigma_1', '\sigma_2', '\sigma_3', '\sigma_4', 'w_1','w_2','w_3','w_4']
 
 
 PE = DirichletProcess(
-    gauss,
+    selected_model,
     names,
     bounds,
     samples,
-    15,
-    45,
+    40,
+    60,
     prior_pars = logPrior,
-    max_a = 10,
-    max_N = 200
+    max_a = 100000,
+    max_g = 100000,
+    max_N = 30
     )
 
 work = cpnest.CPNest(PE,
                     verbose = 2,
                     nlive = 1000,
                     maxmcmc = 1000,
-                    nthreads = 2,
+                    nthreads = 4,
                     output  = out_folder
                     )
 work.run()
 print('log Evidence: {0}'.format(work.NS.logZ))
 
-labels = labels + ['\\log_{10}(\\alpha)', 'N']
-names = names + ['a','N']
+labels = labels + ['\\alpha', '\\gamma', 'N']
+par_names = names
+names = names + ['a','g', 'N']
+#x = np.genfromtxt('/Users/stefanorinaldi/Documents/parametric/DP/nested_samples.dat', names = True).ravel()
 x = work.posterior_samples.ravel()
+#x['b1'] = x['b1']/(x['b1']+x['b2']+x['b3']+x['b4'])
+#x['b2'] = x['b2']/(x['b1']+x['b2']+x['b3']+x['b4'])
+#x['b3'] = x['b3']/(x['b1']+x['b2']+x['b3']+x['b4'])
+#x['b4'] = x['b4']/(x['b1']+x['b2']+x['b3']+x['b4'])
 samps = np.column_stack([x[lab] for lab in names])
 fig = corner.corner(samps,
        labels= [r'${0}$'.format(lab) for lab in labels],
@@ -135,3 +163,15 @@ fig = corner.corner(samps,
        use_math_text=True, truths=true_vals,
        filename=os.path.join(out_folder,'joint_posterior.pdf'))
 fig.savefig(os.path.join(out_folder,'joint_posterior.pdf'), bbox_inches='tight')
+
+p = np.percentile(np.column_stack([x[lab] for lab in par_names]), 50, axis = 0)
+fig = plt.figure()
+ax  = fig.add_subplot(111)
+ax.fill_between(rec['m'], np.exp(rec['95']), np.exp(rec['5']), color = 'lightgreen', alpha = 0.5)
+ax.fill_between(rec['m'], np.exp(rec['84']), np.exp(rec['16']), color = 'aqua', alpha = 0.5)
+ax.plot(rec['m'], np.exp(rec['50']), color = 'r')
+print(*p)
+ax.plot(rec['m'], selected_model(rec['m'], *[50,4]), color = 'k')
+
+fig.savefig(os.path.join(out_folder,'compare_50.pdf'), bbox_inches='tight')
+
