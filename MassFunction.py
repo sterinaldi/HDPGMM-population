557a78bd2ddbe048ef03f04d243af6b408af3aff
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2021-10-07 11:54:00 +0200

    diagnostic flag

--- MassFunction.py
+++ MassFunction.py
@@ -232,24 +232,30 @@ def main():
     '''
     parser = op.OptionParser()
     
+    # Input/Output
     parser.add_option("-i", "--input", type = "string", dest = "events_path", help = "Input folder")
     parser.add_option("-o", "--output", type = "string", dest = "output", help = "Output folder")
-    parser.add_option("--mmin", type = "float", dest = "mmin", help = "Minimum BH mass [Msun]", default = 3.)
-    parser.add_option("--mmax", type = "float", dest = "mmax", help = "Maximum BH mass [Msun]", default = 120.)
-    parser.add_option("--inj_density", type = "string", dest = "inj_density_file", help = "Python module with injected density")
-    parser.add_option("--true_masses", type = "string", dest = "true_masses", help = "Simulated true masses")
     parser.add_option("--optfile", type = "string", dest = "optfile", help = "Options file. Passing command line options overrides optfile. It must contains ALL options")
+    parser.add_option("--inj_density", type = "string", dest = "inj_density_file", help = "Python module with injected density")
+    parser.add_option("--selfunc", dest = "selection_function", help = "Python module with selection function or text file with M_i and S(M_i) for interp1d")
+    parser.add_option("--true_masses", type = "string", dest = "true_masses", help = "Simulated true masses")
+    
+    # Settings
     parser.add_option("--samp_settings", type = "string", dest = "samp_settings", help = "Burnin, samples and step for MF sampling", default = '10,1000,1')
     parser.add_option("--samp_settings_ev", type = "string", dest = "samp_settings_ev", help = "Burnin, samples and step for single event sampling. If None, uses MF settings")
-    parser.add_option("--hyperpriors_ev", type = "string", dest = "hyperpriors_ev", help = "Event hyperpriors (a0, V0). See https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf sec. 6 for reference", default = '1,1')
-    parser.add_option("--alpha", type = "float", dest = "alpha0", help = "Internal (event) concentration parameter", default = 1.)
-    parser.add_option("--gamma", type = "float", dest = "gamma0", help = "External (MF) concentration parameter", default = 1.)
-    parser.add_option("-e", "--processed_events", dest = "process_events", action = 'store_false', default = True, help = "Disables event processing")
     parser.add_option("--icn", dest = "initial_cluster_number", type = "float", help = "Initial cluster number", default = 5.)
     parser.add_option("--nthreads", dest = "n_parallel_threads", type = "int", help = "Number of parallel threads to spawn", default = 8)
+    parser.add_option("-e", "--processed_events", dest = "process_events", action = 'store_false', default = True, help = "Disables event processing")
     parser.add_option("-v", "--verbose", dest = "verbose", action = 'store_true', default = False, help = "Display output")
     parser.add_option("-p", "--postprocessing", dest = "postprocessing", action = 'store_true', default = False, help = "Postprocessing - requires log_rec_prob_mf.txt")
-    parser.add_option("--selfunc", dest = "selection_function", help = "Python module with selection function or text file with M_i and S(M_i) for interp1d")
+    parser.add_option("-d", "--diagnostic", dest = "diagnostic", action = 'store_true', default = False, help = "Run diagnostic routines (Autocorrelation, quasi-convergence)")
+    
+    # Priors
+    parser.add_option("--prior_ev", type = "string", dest = "prior_ev", help = "Parameters for NIG prior (a0, V0). See https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf sec. 6 for reference", default = '1,1')
+    parser.add_option("--mmin", type = "float", dest = "mmin", help = "Minimum BH mass [Msun]", default = 3.)
+    parser.add_option("--mmax", type = "float", dest = "mmax", help = "Maximum BH mass [Msun]", default = 120.)
+    parser.add_option("--alpha", type = "float", dest = "alpha0", help = "Internal (event) initial concentration parameter", default = 1.)
+    parser.add_option("--gamma", type = "float", dest = "gamma0", help = "External (MF) initial concentration parameter", default = 1.)
 
     (options, args) = parser.parse_args()
     
@@ -324,10 +330,11 @@ def main():
                               samp_settings_ev = options.samp_settings_ev,
                               alpha0 = float(options.alpha0),
                               gamma0 = float(options.gamma0),
-                              hyperpriors_ev = options.hyperpriors_ev,
+                              prior_ev = options.prior_ev,
                               m_min = float(options.mmin),
                               m_max = float(options.mmax),
                               verbose = bool(options.verbose),
+                              diagnostic = bool(options.diagnostic)
                               output_folder = options.output,
                               initial_cluster_number = int(options.initial_cluster_number),
                               process_events = bool(options.process_events),
