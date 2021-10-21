289b3c7f6c4abc608c72a252a7dc4e3da93f65a2
stefano.rinaldi <stefano.rinaldi@ligo.org>
2021-10-21 17:51:36 +0200

    install module

--- setup.py
+++ setup.py
@@ -16,17 +16,36 @@ class build_ext(_build_ext):
         __builtins__.__NUMPY_SETUP__ = False
         self.include_dirs.append(numpy.get_include())
 
+with open("requirements.txt") as requires_file:
+    requirements = requires_file.read().split("\n")
+
 ext_modules=[
-             Extension("utils",
-                       sources=["utils.pyx"],
+             Extension("hdpgmm.utils",
+                       sources=[os.path.join("hdpgmm","utils.pyx")],
                        libraries=["m"], # Unix-like specific
                        extra_compile_args=["-O3","-ffast-math"],
-                       include_dirs=[numpy.get_include()]
+                       include_dirs=['hdpgmm', numpy.get_include()]
                        )
              ]
-
+ext_modules = cythonize(ext_modules)
 setup(
-      name = "utils",
+      name = 'hdpgmm/utils',
       ext_modules = cythonize(ext_modules, language_level = "3"),
       include_dirs=[numpy.get_include()]
       )
+
+setup(
+    name = 'hdpgmm',
+    use_scm_version=True,
+    description = '(H)DPGMM: a Hierarchy of DPGMM',
+    author = 'Walter Del Pozzo, Stefano Rinaldi',
+    author_email = 'walter.delpozzo@unipi.it, stefano.rinaldi@phd.unipi.it',
+    url = 'https://git.ligo.org/stefano.rinaldi/hdp-population',
+    python_requires = '>=3.6',
+    packages = ['hdpgmm'],
+    include_dirs = [numpy.get_include()],
+    setup_requires=['numpy', 'cython', 'setuptools_scm'],
+    entry_points={},
+    package_data={"": ['*.c', '*.pyx', '*.pxd']},
+    ext_modules=ext_modules,
+    )
