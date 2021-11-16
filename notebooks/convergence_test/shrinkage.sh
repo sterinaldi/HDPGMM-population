d98c4175a5bec6eabb604468a00973c63965a248
stefano.rinaldi <stefano.rinaldi@ligo.org>
2021-11-16 23:28:18 +0100

    save rec_post

--- notebooks/convergence_test/shrinkage.sh
+++ notebooks/convergence_test/shrinkage.sh
@@ -6,4 +6,5 @@ do
     python single_event.py --optfile notebooks/convergence_test/options_shrinkage.ini --n_samps_dsp $N
     mv notebooks/convergence_test/shrinkage/reconstructed_events/events/gaussian.pdf notebooks/convergence_test/shrinkage/reconstructed_events/events/gaussian_$N.pdf
     mv notebooks/convergence_test/shrinkage/reconstructed_events/events/log_gaussian.pdf notebooks/convergence_test/shrinkage/reconstructed_events/events/log_gaussian_$N.pdf
+    mv notebooks/convergence_test/shrinkage/reconstructed_events/rec_prob/log_rec_prob_gaussian.txt notebooks/convergence_test/shrinkage/reconstructed_events/rec_prob/log_rec_prob_gaussian_$N.txt
 done
